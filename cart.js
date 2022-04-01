02765a310f28d4af8610e811dd96278548e81752
Tanner Vasica <tmanvas06@hotmail.com>
2022-04-01 12:56:50 -0600

    final

---
+++ cart.js
@@ -0,0 +1,99 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+const summedPrice = cart.reduce((acc, curr) => {
+    return acc + curr.price
+}, 0)
+// console.log(summedPrice)
+
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+const calcFinalPrice = (cartTotal, couponValue, tax) => {
+    let sum = cartTotal + (tax * cartTotal)
+    return `Final price is: ${sum - couponValue}`
+}
+
+// console.log(calcFinalPrice(60,0,.07))
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+    The customer object should have a name property, an item property (what they are buying), a creditCard property, and an address.
+
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+const customer = {
+    name: 'Tanner',
+    items: `sandwich`,
+    creditCard: 1234-5678-9012,
+    address: `1234 Nope St.`
+}
---
+++ menu.js
@@ -0,0 +1,217 @@
+///////////////////////////////////////////////
+///////////////////MENU.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code that
+    deals with food objects, arrays of objects
+    and filtering those arrays. 
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Create an object called `pizza` that has 6
+    properties: 
+        - name (string)
+        - price (number)
+        - category (string)
+        - popularity (number)
+        - rating (number)
+        - tags (array of strings)
+
+    Make sure that you give your properties values
+    of the correct data type.
+
+    Note: the category is something like appetizer/entree,
+    the popularity is an overall ranking, the
+    rating is an average of all customer ratings, and
+    for the tags, think of things that a
+    user might filter by, like 'gluten-free' or
+    'kids'
+*/
+
+const pizza ={
+        name: `Tims Pizza`,
+        price: `12.00`,
+        category: `entree`,
+        popularity: 87,
+        rating: 4.3,
+        tags:[`gluten-free`, `kidMenu`,`stuffedCrust`]
+    }
+    
+    
+    
+    
+    //////////////////PROBLEM 2////////////////////
+    /* 
+    Let's print a few values from our pizza object.
+    
+    First, log the popularity of pizza. 
+    Use dot notation to access the value.
+    */
+   
+   console.log(pizza.popularity)
+   
+   
+   /*
+   Second, log the second tag in your pizza's
+   tags array.
+   Use a combination of dots and brackets to
+   get the value.
+   */
+  console.log(pizza.tags[1])
+  
+  
+  /*
+  Third, destructure the price off of the
+  pizza object.
+  
+  Print the value of your new price variable.
+  */
+ 
+ let {price} = pizza
+ console.log(price)
+ 
+ 
+ /*
+ Fourth, and last, destructure the category
+ property.
+ 
+ Print the value of your category variable. 
+ */
+
+let {category} = pizza
+console.log(category)
+
+
+//////////////////PROBLEM 3////////////////////
+/* 
+Create an array with about 5 objects in it.
+The objects should mimic the `pizza` object.
+Call the array `foodArr`.
+
+Make sure that they have slightly different
+values for price, popularity, rating, and
+tags. That way, you'll be able to use this
+data in some functions that you'll write.
+*/
+
+const foodArr =[
+    {
+         name: `Tims Pizza`,
+        price: `13.00`,
+        category: `entree`,
+        popularity: 77,
+        rating: 3.3,
+        tags:[`gluten-free`, `kidMenu`,`breadstick`]
+    },
+    {
+         name: `Tims Pizza`,
+        price: `14.00`,
+        category: `entree`,
+        popularity: 91,
+        rating: 4.7,
+        tags:[`vegan`, `sides`,`thinCrust`]
+    },
+    {
+         name: `Tims Pizza`,
+        price: `12.00`,
+        category: `entree`,
+        popularity: 82,
+        rating: 4.2,
+        tags:[`wholeWheat`, `miniPies`,`dessert`]
+    },
+    {
+         name: `Tims Pizza`,
+        price: `11.00`,
+        category: `entree`,
+        popularity: 67,
+        rating: 2.5,
+        tags:[`pasta`, `salads`,`beer`]
+    },
+    {
+         name: `Tims Pizza`,
+        price: `12.00`,
+        category: `entree`,
+        popularity: 80,
+        rating: 3.9,
+        tags:[`cocktails`, `sides`,`chicagoStyle`]
+    }
+    
+]
+
+
+
+//////////////////PROBLEM 4////////////////////
+/* 
+    Let's filter the food objects according
+    to their tags.
+
+    Write a callback function below that will
+    return only food objects that have a certain tag.
+
+    You can check for any tag that at least 1 of
+    your food objects has.
+*/
+
+
+const filteredFood = foodArr.filter(element => element.tags === foodArr.tags[`sides`])
+
+// console.log(filteredFood)
+
+
+//////////////////PROBLEM 5////////////////////
+/* 
+    Now let's write a function that's a little
+    more flexible than just filtering for one
+    value. We want to be able to filter for 
+    food that has above a certain rating, 
+    below a certain price, or any other combo.
+
+    Write a function called `filterByProperty`
+    that takes in three arguments: `property`, 
+    `number`, and `type. 
+
+    The property will be a string (rating,
+    popularity, or price)
+
+    The number will be the number that you want
+    to compare against 
+
+    The type should be 'above' or 'below' to 
+    indicate whether you want to get foods with
+    values that are above or below the given number
+    for the given property
+
+    Inside the function, create a variable to hold
+    a filtered array
+
+    Use the filter method to filter the foodArr
+
+        In the callback, check if the `type` is `above`, 
+        if it is, return objects whose value for the given
+        property is greater than the `number` passed in
+
+        If the type is `below`, return objects whose
+        value for the given property is less than the 
+        `number` passed in
+    
+    Return the filtered array from the entire function
+*/
+
+const filterByProperty = function(property, number, type){
+    let filltered = foodArr.filter(element => {
+        if(element.rating > 4 && element.popularity > 90)
+        return element[0]
+    }
+})
+
+
+/*
+    Invoke the `filterByProperty` function passing
+    in a value for each paramter.
+
+    You'll have to console.log to see the filtered array
+*/
+
+//CODE HERE
